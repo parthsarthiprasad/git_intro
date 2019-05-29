5daff5ee61d131c1471192a8526f97da29dc4a8b
parthsarthiprasad <mepsp2000@gmail.com>
2019-05-29 20:52:14 +0530

    sheldon greeter

--- file.js
+++ file.js
@@ -1,7 +1,10 @@
 console.log('hello world')
 console.log('tada')
 function greet(name){
-  console.log('hello'+name)
+  console.log('knock knock knock ' + name)
+  console.log('knock knock knock ' + name)
+  console.log('knock knock knock ' + name)
+  
 }
 
 greet('sarthparthi')
